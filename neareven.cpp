0db962e34405ec38a75802bd13c6e098b6616ad3
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-03-03 17:22:42 +0530

    Create neareven.cpp

---
+++ neareven.cpp
@@ -0,0 +1,19 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int n1;
+cout<<"Enter The Number"<<endl;
+cin>>n1;
+if(n1%2==0)
+{
+    n1=n1+2;
+ cout<<n1;
+}
+else
+{ 
+    n1=n1+3;
+ cout<<n1;
+}
+return 0;
+}
