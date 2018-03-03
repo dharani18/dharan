8b0bb51066a09a4e9608beb8c82d18390c686200
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-03-03 16:35:23 +0530

    Create alpnum.cpp

---
+++ alpnum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	clrscr();
+	char ch;
+	int i;
+	cout<<"Enter string: ";
+	cin>>string;
+	if((ch>='a'&& ch<='z') || (ch>='A' && ch<='Z'))&&if(i>='1'&&i<='1000')
+	{
+		cout<<"yes";
+	}
+	else
+	{
+		cout<<"no";
+		return 0;
+	}
+	getch();
