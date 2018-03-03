ec17140a59a31e57c060344bc1e55d5e7e1e9332
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-03-03 16:20:24 +0530

    Create mult.cpp

---
+++ mult.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout<<"enter the values of a,b:";
+cin>>a>>b;
+{ 
+c=a*b;
+}
+if(c%2==0)
+cout<<"even";
+else
+cout<<"odd";
+return 0;
+}
