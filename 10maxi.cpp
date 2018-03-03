aad6fb05b139d6358c22850ac29db9134e9c2b7c
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-03-03 16:54:03 +0530

    Create 10maxi.cpp

---
+++ 10maxi.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+int i,n,x,maximum=0;
+for(i=0;i<=10;++i)
+{
+cout<<“nEnter number “<<i+1<<“:”;
+cin>>x;
+if(x>large)
+large=x;
+}
+cout<<“nnThe max number “<<large;
+return 0;
+}
